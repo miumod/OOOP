bf53d82f9e5f30a74354ec695c40b91ae0953f84
miumod <73156069+miumod@users.noreply.github.com>
2020-12-08 11:33:16 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package lab5;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        WorkWithFile file= new WorkWithFile();
+        file.workWithConsole();
+
+    }
+}
